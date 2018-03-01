1732986c19a2c61201262df6b90e145e847fb48a
hcc123915 <hcc123915@163.com>
2018-03-01 15:50:29 +0800

    new bubbleSortt

--- sortInt/bubbleSort.java
+++ sortInt/bubbleSort.java
@@ -1,18 +1,15 @@
 public static void bubbleSort(int[] array){
-	int temp = 0;
-	int len = array.length;
-	for(int i = 1; i < len; i++){
-		for(int j = 0; j < len-i; j++){
-			if(array[j] > array[j+1]){
-				temp = array[j+1];
-				array[j+1] = array[j];
-				array[j] = temp;
+	for(int i = 0; i < array.length - 1; i++){
+		for(int j = 0; j < array.length - i -1; j++){
+			if(array[j] > array[j + 1]){
+				int temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 			}
 		}
 	}
-	System.out.println(Arrays.toString(array) + "bubbleSort");
+	//System.out.println(Arrays.toString(array) + "bubbleSort");
 	/*System.out.print("[");
 	for(int k = 0; k < len; k++){System.out.print(array[k] + ",");}
 	System.out.println("] bubbleSort");*/
 }
-
